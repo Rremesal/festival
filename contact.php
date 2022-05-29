85ba1ba09150506eb51d87a6f44f12348f602c12
Rremesal <raul.remesal@gmail.com>
2022-05-29 16:47:07 +0200

    email-address added to contactinfo

--- contact.php
+++ contact.php
@@ -2,12 +2,29 @@
 <html lang="en">
 <head>
     <link rel="stylesheet" href="style.css"/>
-    <title>Document</title>
+    <title>Contact</title>
 </head>
 <body>
     <?php require("menu.php");?>
     <div class="content" id="contactpage">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d412181.1227616094!2d-87.0654449254165!3d36.18686829495643!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8864ec3213eb903d%3A0x7d3fb9d0a1e9daa0!2sNashville%2C%20Tennessee%2C%20Verenigde%20Staten!5e0!3m2!1snl!2snl!4v1653468914065!5m2!1snl!2snl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <div id="contactImage">
+        </div>
+        <div id="contact-box">
+            <div id="contactInfo">
+                <h2>Contact Info</h2>
+                <ul>
+                    <li>Phone Number: 0619321946 </li>
+                    <li>E-mail Address: amerijck.info@gmail.com</li>
+                </ul>
+            </div>
+            <div id="googleMaps">
+                <h2>Location</h2>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12883.851550098912!2d-86.77373597644156!3d36.16745771787598!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8864664fe4bf4197%3A0xf9c5bc49669894e9!2sNissan%20Stadium!5e0!3m2!1snl!2snl!4v1653557754443!5m2!1snl!2snl" 
+                    width="300" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+                </iframe>
+            </div>
+        </div>
+        
 
     </div>
 </body>
