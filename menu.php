fff09107714019cb465bc12afadb5d8a2cdbd181
Rremesal <raul.remesal@gmail.com>
2022-06-08 17:55:13 +0200

    changed menu dropdown 'logged in as' value

--- menu.php
+++ menu.php
@@ -1,20 +1,17 @@
 
 <a href="index.php"><img id="logo" src="images/logo.png"/></a>
-<a><img id="shoppingcartLogo" src="images/shoppingcart2.png"/></a>
-<?php if(isset($_SESSION['user'])) {
+<a href="order.php"><img id="shoppingcartLogo" src="images/shoppingcart2.png"/></a>
+<?php if(isset($_SESSION['user_name'])) {
 ?>      <a onclick="showDropDown()"><img id="userLogo" src="images/user_logo.png"/></a>
 <?php } ?>
 
 <div id="dropdown">
-    <div><a href="login.php"> <?php if(!isset($_SESSION['user'])) echo "Sign in"; else echo "Logged in as: ".$_SESSION['user']; ?></a></div>
-    <?php  if(isset($_SESSION['user'])) {
+    <div><a href="login.php"> <?php if(!isset($_SESSION['user_name'])) echo "Sign in"; else echo "Logged in as: ".$_SESSION['user_name']; ?></a></div>
+    <?php  if(isset($_SESSION['user_name'])) {
     ?>     <a href="logout.php">Log out</a>
     <?php
         }
     ?>
-
-
-
 </div>
 <div id="menu"> 
     <div id="a-items">
